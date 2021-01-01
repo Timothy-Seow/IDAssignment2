b13d8a17f2f9e8d9c3b89425452d6bf2b493a05d
Timothy Seow <s10204778@connect.np.edu.sg>
2021-01-01 13:56:33 +0800

    displayed item image and info on item page

--- script.js
+++ script.js
@@ -2,30 +2,25 @@ fetch('http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json')
     .then(res => res.json())
     .then(function(data){
         let champion = data.data;
-        console.log(champion); 
+        console.log(champion);
 
         var champlist = [];
         var content1 = "";
         var content2 = "";
-        var content3 = "";
         $.each(champion, function(key, obj) {
             const imagelink = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${key}_0.jpg`
-            console.log(key + " , " + obj.title);
             content1 = key + " , " + obj.title;
             content2 = obj.blurb;
-            content3 = obj.image.full;
             champlist.push([content1,content2,imagelink])
             });
 
-        console.log(champlist)
-
         $.each(champlist, function(index, value){
-            $('#container').append(
+            $('#champion').append(
                 $('<div/>')
                 .addClass("card")
                 .append(
                 $('<img/>')
-                .addClass("card-image")
+                .addClass("champion-card-image")
                 .attr("src", value[2])
                 .attr("alt", value[0]),
                 $('<div/>')
@@ -39,5 +34,47 @@ fetch('http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/champion.json')
                 .text(value[1])
             )));
         });
+    })
+
+
+fetch('http://ddragon.leagueoflegends.com/cdn/10.25.1/data/en_US/item.json')
+    .then(res => res.json())
+    .then(function(data){
+        let item = data.data;
+        console.log(item);
+
+
+ 
+        var itemlist = [];
+        var value1 = "";
+        var value2 = "";
+        $.each(item, function(key, obj) {
+            const imagelink = `https://ddragon.leagueoflegends.com/cdn/10.25.1/img/item/${key}.png`
+            value1 = obj.name;
+            value2 = obj.plaintext;
+            itemlist.push([value1,value2,imagelink])
+            });
+
+        $.each(itemlist, function(index, value){
+            $('#item').append(
+                $('<div/>')
+                .addClass("card")
+                .append(
+                $('<img/>')
+                .addClass("item-card-image")
+                .attr("src", value[2])
+                .attr("alt", value[0]),
+                $('<div/>')
+                .addClass("card-body")
+                .append(
+                $('<h5/>')
+                .addClass("card-title")
+                .text(value[0]),
+                $('<p/>')
+                .addClass("card-text")
+                .text(value[1])
+            )));
+        });
+
     })
 
